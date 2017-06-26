7be4159eef2c55f0a2acaf4bc4e2155b7f3e0362
liuchun <xiaohalc@qq.com>
2017-06-26 17:32:48 +0800

    Create SyncQueue.js

---
+++ SyncQueue.js
@@ -0,0 +1,29 @@
+
+  // 同步队列，添加到该队列的方法都会同步执行，使用new来创建多个同步队列
+  function SyncQueue() {
+    this.syncQueue = [];
+    this.tickTimer = null;
+  }
+
+  SyncQueue.prototype = {
+    nextTick: function () {
+      var self = this;
+      if (!self.tickTimer && self.syncQueue && self.syncQueue.length) {
+        self.tickTimer = setTimeout(function () {
+          if (self.syncQueue && self.syncQueue.length) {
+            $.when(self.syncQueue.shift()()).always(function () {
+              self.tickTimer = null;
+              self.nextTick();
+            });
+          } else {
+            self.tickTimer = null;
+          }
+        }, 1);
+      }
+    },
+    // 添加同步加载任务
+    addSyncTask: function (fn) {
+      this.syncQueue.push(fn);
+      this.nextTick();
+    }
+  };
